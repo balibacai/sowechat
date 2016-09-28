6780ea49196dcca44b579aa19501db0e0bbd2d4f
mistcheng <guangchao007@126.com>
2016-09-28 18:32:30 +0800

    init

---
+++ app/Console/Commands/WechatListen.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Exceptions\Wechat\WebApi;
+use Illuminate\Console\Command;
+
+class WechatListen extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'wechat:listen';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'simulate web wechat and listening message';
+
+    /**
+     * @var \App\Exceptions\Wechat\WebApi
+     */
+    protected $api;
+
+    /**
+     * Create a new command instance.
+     *
+     * @param WebApi $api
+     * @return void
+     */
+    public function __construct(WebApi $api)
+    {
+        $this->api = $api;
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        dd($this->api->getUUID());
+    }
+}
---
+++ app/Extensions/Wechat/WebApi.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Exceptions\Wechat;
+
+use Exception;
+use GuzzleHttp\Client;
+
+class WebApi
+{
+
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    protected $client;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+    }
+
+    protected function request($method, $uri, array $options = [])
+    {
+        $response = $this->client->request($method, $uri, $options);
+
+        if ($response->getStatusCode() != '200') {
+            throw new Exception('Request Error');
+        }
+
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * 生成当前时间戳（毫秒）
+     * @return int
+     */
+    protected function getTimeStamp()
+    {
+        return intval(microtime(true) * 1000);
+    }
+
+    /**
+     * 返回uuid
+     * @return string uuid
+     * @throws Exception
+     */
+    public function getUUID()
+    {
+        $url = 'https://login.weixin.qq.com/jslogin';
+        $response = $this->request('GET', $url, [
+            'query' => [
+                'appid' => 'wx782c26e4c19acffb',
+                'fun' => 'new',
+                'lang' => 'zh_CN',
+                '_' => $this->getTimeStamp(),
+            ]
+        ]);
+
+        preg_match('|window.QRLogin.code = (\d+); window.QRLogin.uuid = "(\S+?)";|', $response, $matches);
+
+        if (empty($matches) || count($matches) != 3 || intval($matches[1]) != 200) {
+            throw new Exception('get uuid parse error');
+        }
+
+        return $matches[2];
+    }
+
+    public function getQRCode($uuid)
+    {
+        return 'https://login.weixin.qq.com/qrcode/' . $uuid;
+    }
+
+    /**
+     * 监听用户扫码登录
+     * @param $uuid
+     * @return null|string 成功会返回redirect_uri，否则返回null
+     * @throws Exception
+     */
+    public function loginListen($uuid)
+    {
+        $url = 'https://login.wx2.qq.com/cgi-bin/mmwebwx-bin/login';
+        $response = $this->request('GET', $url, [
+            'query' => [
+                'uuid' => $uuid,
+                'tip' => 0,
+                '_' => $this->getTimeStamp(),
+            ]
+        ]);
+
+        preg_match('|window.code=(\d+);|', $response, $matches);
+
+        if (empty($matches) || count($matches) != 2) {
+            return null;
+        }
+
+        $code = intval($matches[1]);
+
+        if ($code == 200) {
+            preg_match('|window.code=200;window.redirect_uri="(\S+?)";|', $response, $matches);
+        }
+
+        if (empty($matches) || count($matches) != 2) {
+            throw new Exception('login success parse error');
+        }
+
+        return $matches[1];
+    }
+}
