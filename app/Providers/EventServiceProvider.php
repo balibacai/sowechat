516fe4524737da9e5204f3b74956fe87e4879b3a
mistcheng <guangchao007@126.com>
2016-10-04 23:49:52 +0800

    change class name

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -14,7 +14,7 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         'App\Events\WechatMessageEvent' => [
-            'App\Listeners\WechatMessageListener',
+            'App\Listeners\SaveWechatMessageListener',
         ],
     ];
 
