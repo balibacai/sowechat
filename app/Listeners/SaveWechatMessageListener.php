3cea5796bd7341df14d178923b18f8b186799071
mistcheng <guangchao007@126.com>
2016-10-07 21:15:20 +0800

    add remark name

--- app/Listeners/SaveWechatMessageListener.php
+++ app/Listeners/SaveWechatMessageListener.php
@@ -34,13 +34,16 @@ class SaveWechatMessageListener
      */
     public function handle(WechatMessageEvent $event)
     {
+        $from_name = array_get($event->from, 'RemarkName') ?: array_get($event->from, 'NickName');
+        $to_name = array_get($event->to, 'RemarkName') ?: array_get($event->to, 'NickName');
+
         $message = new WechatMessage([
             'type' => MessageType::getType($event->type),
             'content' => is_array($event->value) ? json_encode($event->value, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES) : $event->value,
             'from_user_name' => $event->from ? array_get($event->from, 'UserName') : null,
-            'from_user_nick' => $event->from ? array_get($event->from, 'RemarkName', array_get($event->from, 'NickName')) : null,
+            'from_user_nick' => $from_name,
             'to_user_name' => $event->to ? array_get($event->to, 'UserName') : null,
-            'to_user_nick' => $event->to ? array_get($event->to, 'RemarkName', array_get($event->to, 'NickName')) : null,
+            'to_user_nick' => $to_name,
             'info' => json_encode($event->info, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES),
         ]);
         $message->save();
