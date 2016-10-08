62c621a51a2e14efa36c5ef58fe025337b6184e6
mistcheng <guangchao007@126.com>
2016-10-08 09:34:16 +0800

    modify migrations

--- database/migrations/2016_10_04_163951_create_jobs_table.php
+++ database/migrations/2016_10_04_163951_create_jobs_table.php
@@ -15,7 +15,7 @@ class CreateJobsTable extends Migration
     {
         Schema::create('jobs', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('queue');
+            $table->string('queue', 191);
             $table->longText('payload');
             $table->tinyInteger('attempts')->unsigned();
             $table->unsignedInteger('reserved_at')->nullable();
